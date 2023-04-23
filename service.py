2ffda5e4f827ba2b071fba90ded5486b01eb46b6
liuwenran <448073814@qq.com>
2023-04-23 16:28:59 +0800

    incrementally change mask

--- service.py
+++ service.py
@@ -1,12 +1,11 @@
-import numpy as np 
+import numpy as np
 import cv2
-from PIL import Image, ImageDraw
-import torch 
-from torch.cuda.amp import autocast
+from PIL import Image
+import torch
 import gradio as gr
 
-from segment_anything import build_sam, SamAutomaticMaskGenerator 
-from diffusers import StableDiffusionInpaintPipeline 
+from segment_anything import build_sam, SamAutomaticMaskGenerator
+from diffusers import StableDiffusionInpaintPipeline
 
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -23,6 +22,14 @@ sd_pipe = StableDiffusionInpaintPipeline.from_pretrained(
 )
 sd_pipe = sd_pipe.to(device)
 print('load sd model.')
+
+# mask_generator = None
+# sd_pipe = None
+
+global origin_image_path
+origin_image_path = None
+global incremental_mask
+incremental_mask = None
 
 
 def crop_image_pillow(img, divide=8):
@@ -48,10 +55,10 @@ def crop_image(img, divide=8):
     height, width, c = img.shape
     if width > 960:
         img = cv2.resize(img, (960, int(960 / width * height)))
-        width, height, c = img.shape
+        height, width, c = img.shape
     if height > 960:
         img = cv2.resize(img, (int(960 / height * width), 960))
-        width, height, c = img.shape
+        height, width, c = img.shape
 
     top = height - height // divide * divide
     left = width - width // divide * divide
@@ -60,68 +67,89 @@ def crop_image(img, divide=8):
 
 
 def segment(
-    pathes: str,
     text_prompt: str,
-    use_drawed_mask: bool
 ):
-    draw_mask_path = pathes['mask']
+    global origin_image_path
+    original_image = Image.open(origin_image_path)
+    original_image = crop_image_pillow(original_image)
+
+    global incremental_mask
+    output_draw_mask = incremental_mask * 255
+    output_draw_mask = np.expand_dims(output_draw_mask, axis=2)
+    output_draw_mask = np.repeat(output_draw_mask, repeats=3, axis=2)
+    output_draw_mask = Image.fromarray(output_draw_mask)
+
+    gen_image = sd_pipe(prompt=text_prompt,
+                        image=original_image,
+                        mask_image=output_draw_mask,
+                        width=original_image.size[0],
+                        height=original_image.size[1]).images[0]
+
+    return gen_image
+
+
+def clear_cache():
+    global origin_image_path
+    origin_image_path = None
+    global incremental_mask
+    incremental_mask = None
+
+    return None, None, None, None
+
+
+def preview(pathes, use_drawed_mask):
     image_path = pathes['image']
+    draw_mask_path = pathes['mask']
+
+    global origin_image_path
+    if origin_image_path is None:
+        origin_image_path = image_path
 
     draw_mask = cv2.imread(draw_mask_path)
     draw_mask = crop_image(draw_mask)
     gray = cv2.cvtColor(draw_mask, cv2.COLOR_BGR2GRAY)
     draw_mask = gray > 0
+    draw_mask = draw_mask.astype('uint8')
 
-    segmentation_masks = []
-    if use_drawed_mask:
-        segmentation_mask_image = Image.fromarray(draw_mask.astype('uint8') * 255)
-        segmentation_masks.append(segmentation_mask_image)
-    else:
+    if not use_drawed_mask:
         image = cv2.imread(image_path)
         image = crop_image(image)
-        
+
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         masks = mask_generator.generate(image)
 
         indices = []
         for i, mask in enumerate(masks):
-            bitwise_res = cv2.bitwise_and(mask['segmentation'].astype('uint8'), draw_mask.astype('uint8'))
+            bitwise_res = cv2.bitwise_and(mask['segmentation'].astype('uint8'), draw_mask)
             if np.sum(bitwise_res) > 0:
                 indices.append(i)
 
         for seg_idx in indices:
-            segmentation_mask_image = Image.fromarray(masks[seg_idx]["segmentation"].astype('uint8') * 255)
-            segmentation_masks.append(segmentation_mask_image)
+            draw_mask = cv2.bitwise_or(masks[seg_idx]["segmentation"].astype('uint8'), draw_mask)
 
-    original_image = Image.open(image_path)
-    original_image = crop_image_pillow(original_image)
+    global incremental_mask
+    if incremental_mask is None:
+        incremental_mask = draw_mask
+    else:
+        incremental_mask = cv2.bitwise_or(incremental_mask, draw_mask)
 
-    overlay_image = Image.new('RGBA', original_image.size, (0, 0, 0, 255))
-    overlay_color = (255, 255, 255, 0)
+    output_draw_mask = incremental_mask * 255
+    output_draw_mask = np.expand_dims(output_draw_mask, axis=2)
+    output_draw_mask = np.repeat(output_draw_mask, repeats=3, axis=2)
+    output_draw_mask = Image.fromarray(output_draw_mask)
 
-    draw = ImageDraw.Draw(overlay_image)
-    for segmentation_mask_image in segmentation_masks:
-        draw.bitmap((0, 0), segmentation_mask_image, fill=overlay_color)
-
-    mask_image = overlay_image.convert("RGB")
-
-    mask_image_np = np.asarray(mask_image)
-    mask_image_gray = cv2.cvtColor(mask_image_np, cv2.COLOR_BGR2GRAY)
-    mask_image_binary = mask_image_gray > 0
-    mask_image_binary = 1 - mask_image_binary.astype('uint8')
+    mask_image_binary = 1 - incremental_mask
     nb = np.expand_dims(mask_image_binary, axis=2)
     nm = np.repeat(nb, repeats=3, axis=2)
-    original_image_np = np.asarray(original_image)
-    original_image_masked = original_image_np * nm
-    original_image_masked = Image.fromarray(original_image_masked)
-    
-    gen_image = sd_pipe(prompt=text_prompt,
-                        image=original_image,
-                        mask_image=mask_image,
-                        width=original_image.size[0],
-                        height=original_image.size[1]).images[0] 
 
-    return mask_image, original_image_masked, gen_image 
+    image = cv2.imread(image_path)
+    image = crop_image(image)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+    original_image_masked = image * nm
+    original_image_masked = Image.fromarray(original_image_masked)
+
+    return original_image_masked, output_draw_mask, original_image_masked
 
 
 block = gr.Blocks().queue()
@@ -130,45 +158,29 @@ with block:
         gr.Markdown('## Inpainting with Stable Diffusin and SAM')
     with gr.Row():
         with gr.Column():
-            image_input = gr.Image(type="filepath", tool='sketch')
-            prompt = gr.Textbox(label='Prompt')
+            image_input = gr.Image(type="filepath", tool='sketch', brush_radius=20)
             use_drawed_mask = gr.Checkbox(label='Use drawed mask.', value=False)
+            preview_button = gr.Button(value='Generate Mask', label='Generate Mask')
+            prompt = gr.Textbox(label='Prompt')
             run_button = gr.Button(label='Run')
-            # with gr.Accordion('Advanced options', open=False):
-            #     a_prompt = gr.Textbox(
-            #         label='Added Prompt',
-            #         value='best quality, extremely detailed')
-            #     n_prompt = gr.Textbox(
-            #         label='Negative Prompt',
-            #         value='longbody, lowres, bad anatomy, bad hands, ' +
-            #         'missing fingers, extra digit, fewer digits, '
-            #         'cropped, worst quality, low quality')
-            #     controlnet_conditioning_scale = gr.Slider(
-            #         label='Control Weight',
-            #         minimum=0.0,
-            #         maximum=2.0,
-            #         value=0.7,
-            #         step=0.01)
-            #     width = gr.Slider(
-            #         label='Image Width',
-            #         minimum=256,
-            #         maximum=768,
-            #         value=512,
-            #         step=64)
-            #     height = gr.Slider(
-            #         label='Image Width',
-            #         minimum=256,
-            #         maximum=768,
-            #         value=512,
-            #         step=64)
+            image_out = gr.Image(label='Inpainting Result', elem_id='inpainting-output').style(height=524)
 
         with gr.Column():
-            mask_out = gr.Image(label='Mask Result', elem_id='mask-output')
-            masked_image = gr.Image(label='Masked image Result', elem_id='masked-img-output')
-            image_out = gr.Image(label='Inpainting Result', elem_id='inpainting-output')
+            preview_image = gr.Image(type="filepath", tool='sketch').style(height=524)
+            mask_out = gr.Image(label='Mask Result', elem_id='mask-output').style(height=524)
 
-    ips = [image_input, prompt, use_drawed_mask]
-    run_button.click(fn=segment, inputs=ips, outputs=[mask_out, masked_image, image_out])
+    with gr.Row():
+        clear_btn = gr.Button(value='Clear Cache', label='Clear Cache')
 
-block.launch(server_name='0.0.0.0')
+    preview_btn_inpus = [image_input, use_drawed_mask]
+    preview_btn_outputs = [preview_image, mask_out, image_input]
+    preview_button.click(fn=preview,
+                         inputs=preview_btn_inpus,
+                         outputs=preview_btn_outputs)
+
+    run_button.click(fn=segment, inputs=[prompt], outputs=[image_out])
+
+    clear_btn.click(fn=clear_cache, outputs=[preview_image, mask_out, image_input, image_out])
+
+block.launch(server_name='0.0.0.0', server_port=8151, share=False)
 
